b7d817440990cd094c61672e4c53b42064d2f61a
Renato <paulo.borupdebakker@dci-student.org>
2025-04-02 20:46:36 +0200

    more updates

---
+++ main.ts
@@ -0,0 +1,17 @@
+import './style.css'
+
+
+const exclamation = document.querySelector(".exclamation") as HTMLElement;
+
+if (exclamation) {
+    setInterval(() => {
+        exclamation.style.visibility = 
+            exclamation.style.visibility === "hidden" ? "visible" : "hidden";
+    }, 1000);
+}
+
+
+
+
+
+
