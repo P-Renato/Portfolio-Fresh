fcba159d8f3500a652929dfc3a82ae508f049be3
Renato <paulo.borupdebakker@dci-student.org>
2025-04-03 11:11:22 +0200

    updates

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ if (exclamation) {
     setInterval(() => {
         exclamation.style.visibility = 
             exclamation.style.visibility === "hidden" ? "visible" : "hidden";
-    }, 1000);
+    }, 800);
 }
 
 
